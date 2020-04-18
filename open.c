d1210a454491c28c8b8e0c8a24fc3ff321867f47
JeongHo <wwwkang8@gmail.com>
2020-04-18 20:59:14 +0900

    lowlevel file

--- open.c
+++ open.c
@@ -6,42 +6,47 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-
 static int append_file(void) {
+
 	int fd;
 
 	fd = open("datafile", O_WRONLY | O_APPEND);
-	if (fd == -1) {
 
-		printf("append() error\n");
+	if (fd == -1) {
+		printf("open() error\n");
 		return -1;
 
 	}
-	dprintf(fd, "hello again..\n");
 
+	dprintf(fd, "append file hehe\n");
 	close(fd);
+
 	return 0;
+
+
 }
 
-
 static int trunc_file(void) {
+
 	int fd;
 
 	fd = open("datafile", O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
 	if (fd == -1) {
 
-		printf("trunc() error\n");
+		printf("open() error\n");
 		return -1;
 
 	}
-	dprintf("byebye file !!\n");
 
+	dprintf("trunc_file");
 	close(fd);
+
 	return 0;
 }
 
-
 static int write_file(void) {
+
 	int fd;
 
 	fd = open("datafile", O_WRONLY | O_CREAT, 0600);
@@ -49,34 +54,37 @@ static int write_file(void) {
 
 		printf("open() error\n");
 		return -1;
-
 	}
-	dprintf("hello world!! %d\n", 123);
+
+	dprintf("hello world!!");
 
 	close(fd);
+
 	return 0;
+
 }
 
 int main(int argc, char **argv) {
+
 	if (write_file()) {
 
 		printf("write_file() error\n");
 		return -1;
+
 	}
 
 	if (trunc_file()) {
-
 		printf("trunc_file() error\n");
 		return -1;
+
 	}
 
 	if (append_file()) {
 
-		printf("append_file() error\n");
+		printf("trunc_file() error \n");
 		return -1;
+
 	}
-
-
 
 
 	return 0;
